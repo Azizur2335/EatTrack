28ac48ccd051256b0bc592fbb8a3904e993900f7
Azizur2335 <azizurrifki2335@gmail.com>
2026-06-03 01:01:44 +0800

    update menu-menu owner

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -11,14 +11,34 @@ class OwnerController extends Controller
 {
     public function beranda()
     {
+        $periode = request('periode', 'day');
         $restaurant = Restaurant::where('owner_id', auth()->id())->first();
-        return view('Owner/beranda_owner', compact('restaurant'));
+        return view('Owner/beranda_owner', compact('restaurant', 'periode'));
     }
 
     public function kelolaMenu()
     {
-        $restaurant = Restaurant::where('owner_id', auth()->id())->get();
+        $restaurant = Restaurant::where('owner_id', auth()->id())->first();
         $menus = $restaurant ? Menu::where('restaurant_id', $restaurant->id)->get() : collect();
         return view('Owner/kelola_menu', compact('restaurant', 'menus'));
+    }
+
+    public function konfirmasiBook()
+    {
+        $restaurant = Restaurant::where('owner_id', auth()->id())->first();
+        $reservasi = $restaurant ? Reservation::where('restaurant_id', $restaurant->id)->get() : collect();
+        return view('Owner/konfirmasi_book', compact('restaurant', 'reservasi'));
+    }
+
+    public function promo()
+    {
+        $restaurant = Restaurant::where('owner_id', auth()->id())->first();
+        return view('Owner/promo_owner', compact('restaurant'));
+    }
+
+    public function profil()
+    {
+        $user = auth()->user();
+        return view('Owner/profil_owner', compact('user'));
     }
 }
---
+++ resources/views/Owner/promo_owner.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>Halaman ini sedang dalam pengembangan</h1>
+    <a href="/dashboard_owner">Kembali ke Dashboard</a>
+</body>
+</html>
