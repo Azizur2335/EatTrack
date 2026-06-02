70ff2309310fe21ac4c2253520b850f8d027255b
Azizur2335 <azizurrifki2335@gmail.com>
2026-06-03 01:29:30 +0800

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,7 +38,7 @@ class DatabaseSeeder extends Seeder
         ];
 
         foreach ($users as $user) {
-            User::create($user);
+            User::firstOrCreate(['email' => $user['email']], $user);
         }
     }
 }
