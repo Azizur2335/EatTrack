c9bd5a70aae497dd8e6c55bc477eb59434d259dc
tegarpng <tegarbijanta@gmail.com>
2026-06-09 09:50:38 +0800

    menambahkan index untuk jumlah user

---
+++ app/Http/Controllers/indexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Restaurant;
+use App\Models\Reservation;
+
+class IndexController extends Controller
+{
+	// Controller
+	public function index() {
+		return view('index', [
+			'totalUsers'       => User::count(),
+			'totalRestaurants' => Restaurant::count(),
+		]);
+	}
+}
